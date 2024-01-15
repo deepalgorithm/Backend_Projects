f5d5785c21a00e8582251f7198801a41b9de1006
deepalgorithm <hamin.lim@gmail.com>
2024-01-15 15:42:45 +0900

    Common Spec for the API

--- Backend_Delivery_Sevice/service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ Backend_Delivery_Sevice/service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -3,12 +3,14 @@ package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
@@ -19,12 +21,14 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public AccountMeResponse me(){
+    public Api<AccountMeResponse> me(){
 
-        return AccountMeResponse.builder()
+        var response = AccountMeResponse.builder()
                 .name("홍길동")
                 .email("A@gmail.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
+
+        return Api.OK(response);
     }
 }
