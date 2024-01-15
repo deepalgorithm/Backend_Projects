b96f5f7f31d9b19c31e701fb146a832b62e64e2f
deepalgorithm <hamin.lim@gmail.com>
2024-01-15 23:31:12 +0900

    Exception Handler

--- Backend_Delivery_Sevice/service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ Backend_Delivery_Sevice/service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -4,6 +4,9 @@ package org.delivery.api.account;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.api.common.api.Api;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.UserErrorCode;
+import org.delivery.api.common.exception.ApiException;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +31,15 @@ public class AccountApiController {
                 .email("A@gmail.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
+
+        var str = "안녕하세요";
+        var age = 0;
+        try{
+            Integer.parseInt(str);
+        }catch (Exception e){
+            throw new ApiException(ErrorCode.SERVER_ERROR, e , "사용자 Me 호출시 에러 발생");
+        }
+
 
         return Api.OK(response);
     }
